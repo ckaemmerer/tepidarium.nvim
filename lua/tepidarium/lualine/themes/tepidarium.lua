55591f42432addf619ec6fb9623fea8db1bbac65
ckaemmerer <ck@homelab.internal>
2026-04-23 19:37:06 +0200

    [minor] swap base colors

--- lua/tepidarium/lualine/themes/tepidarium.lua
+++ lua/tepidarium/lualine/themes/tepidarium.lua
@@ -1,43 +1,38 @@
 local P = require("tepidarium.palette")
+local D = require("tepidarium.palette_derived")
 
 local M = {}
 
 M.normal = {
-  a = { bg = P.blue, fg = P.black },
-  b = { bg = P.black, fg = P.blue },
-  c = {
-    bg = P.bg,
-    fg = P.ui_muted,
-  },
-  x = {
-    bg = P.bg,
-    fg = P.ui_muted,
-  },
+  a = { bg = P.blue, fg = P.bg },
+  b = { bg = P.bright_black, fg = P.blue },
+  c = { bg = P.bg, fg = D.ui_muted },
+  x = { bg = P.bg, fg = D.ui_muted },
 }
 
 M.insert = {
-  a = { bg = P.green, fg = P.black },
-  b = { bg = P.black, fg = P.green },
+  a = { bg = P.green, fg = P.bg },
+  b = { bg = P.bright_black, fg = P.green },
 }
 
 M.command = {
-  a = { bg = P.yellow, fg = P.black },
-  b = { bg = P.black, fg = P.yellow },
+  a = { bg = P.yellow, fg = P.bg },
+  b = { bg = P.bright_black, fg = P.yellow },
 }
 
 M.visual = {
-  a = { bg = P.magenta, fg = P.black },
-  b = { bg = P.black, fg = P.magenta },
+  a = { bg = P.magenta, fg = P.bg },
+  b = { bg = P.bright_black, fg = P.magenta },
 }
 
 M.replace = {
-  a = { bg = P.red, fg = P.black },
-  b = { bg = P.black, fg = P.red },
+  a = { bg = P.red, fg = P.bg },
+  b = { bg = P.bright_black, fg = P.red },
 }
 
 M.inactive = {
-  a = { bg = P.black, fg = P.yellow },
-  b = { bg = P.black, fg = P.black },
+  a = { bg = P.bright_black, fg = D.ui_muted },
+  b = { bg = P.bg, fg = P.bright_black },
 }
 
 return M
