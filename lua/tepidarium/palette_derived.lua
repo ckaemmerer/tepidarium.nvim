36e5872f782d36b06b060a15edbfb24052c1b988
ckaemmerer <ck@homelab.internal>
2026-04-23 19:51:51 +0200

    [minor] color tuning

--- lua/tepidarium/palette_derived.lua
+++ lua/tepidarium/palette_derived.lua
@@ -2,9 +2,9 @@ local P = require("tepidarium.palette")
 local H = require("tepidarium.color_helper")
 
 local D = {
-  namespace_green = H.darken(P.green, 35),
-  param_blue      = H.blend(P.bright_blue, 0.25, P.fg),
-  operator_purple = H.midpoint(P.magenta, P.blue),
+  namespace_green = H.darken(P.bright_green, 35),
+  param_blue      = P.fg,
+  operator_purple = P.purple,
   special_string  = H.midpoint(P.orange, P.red, 0.35),
   ui_surface      = H.lighten(P.bg, 8),
   ui_border       = H.lighten(P.bg, 15),
